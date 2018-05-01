8146c1c3be5c51eb6c76d586d15a81bd63f32edf
Masa Wu <masastirn23@gmail.com>
2018-05-01 10:47:15 +0200

    added loop for btns, highliting class corrected

--- js/performers.js
+++ js/performers.js
@@ -1,9 +1,6 @@
 const btnTemplate = document.querySelector("#btn-template").content;
 const btnVenues = document.querySelector("#btn-venues");
 
-//const headerTemplate = document.querySelector("#header-template").content;
-//const headerVenues = document.querySelector("#header-venue");
-
 const concTemplate = document.querySelector("#concert-template").content;
 const concVenues = document.querySelector("#concert-venues");
 
@@ -12,9 +9,9 @@ function fetchVenues() {
         let urlParams = new URLSearchParams(window.location.search)
         let cat = urlParams.get("category");
         fetch("http://test.masawudesign.dk/wp-json/wp/v2/venues?_embed&categories="+cat)
-            //respons back
+            //respons back, expect json
             .then(e => e.json())
-            //now we have the data, we can do with it whatever we want
+            //now we have the venue data, we can do with it whatever we want
             .then(showVenues)
     }
 
@@ -29,46 +26,34 @@ function showVenues(data) {
     }
 
 function showSingleVenue(aVenue) {
-
     //fetch content for the top 3 buttons of the concert venues
     // make the copy of the template
     let clone = btnTemplate.cloneNode(true);
 
     let venText = aVenue.slug;
-    let venImgs = aVenue._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
 
     //CONCERT BUTTONS ON THE TOP OF CONCERT VENUE PAGE
     if(aVenue.acf.concert===true){
-
-        //clone.querySelector(".venue-imgs").setAttribute("src", venImgs)
-    //button text
+        //console.log(aVenue.acf.concert)
         clone.querySelector(".venue-text").textContent=venText.toUpperCase();
-
-//if(aVenue.slug==="musikcafeen"){
-//
-//
-//        clone.querySelector(".venue-text").textContent=venText.toUpperCase().substring(0,5);
-//        const cafeen = document.createElement("div");
-//        cafeen.textContent=venText.toUpperCase().substring(5,11);
-//
-//
-//        clone.querySelector(".venue-text").append(cafeen);
-//    }
 }
     //THEATRE BUTTONS ON THE TOP OF THEATRE VENUE PAGE
     if(aVenue.acf.theatre===true){
-
-        //clone.querySelector(".venue-imgs").setAttribute("src", venImgs)
-    //button text
         clone.querySelector(".venue-text").textContent=venText.toUpperCase();
 }
     //BIOGRAF BUTTONS ON THE TOP OF THEATRE VENUE PAGE
     if(aVenue.acf.cinema===true){
-
-      //  clone.querySelector(".venue-imgs").setAttribute("src", venImgs)
-    //button text
         clone.querySelector(".venue-text").textContent=venText.toUpperCase();
 }
+//    if (clone.querySelector(".venue-text").classList.contains("white")){
+//        clone.querySelector(".venue-text").classList.remove("white")
+//            }
+//    clone.querySelector(".venue-text").addEventListener("click", (e)=>{
+//        //console.log(aVenue.slug)
+//        //console.log(aVenue.title.rendered)
+//        e.target.parentElement.classList.toggle("highlited");
+//        e.target.classList.toggle("white")
+//    })
     //take each clone and added it to the html
      btnVenues.appendChild(clone);
 
@@ -76,15 +61,14 @@ function showSingleVenue(aVenue) {
 
 
     //fetch the content for each venue
-
+    // make the copy of the template
     let clone2 = concTemplate.cloneNode(true);
+    let venImgs = aVenue._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+
     clone2.querySelector("h1").textContent = aVenue.title.rendered;
     clone2.querySelector(".desciption").innerHTML = aVenue.content.rendered;
-
-    //console.log(aVenue)
-
     clone2.querySelector(".conc-img").setAttribute("src", venImgs);
-     clone2.querySelector(".price").textContent = aVenue.acf.rental_cost;
+    clone2.querySelector(".price").textContent = aVenue.acf.rental_cost;
     clone2.querySelector(".floor").textContent = aVenue.acf.floor;
     clone2.querySelector(".usage").textContent = aVenue.acf.usage;
     clone2.querySelector(".facilitates").textContent = aVenue.acf.facilitates_on_the_venue;
@@ -113,78 +97,84 @@ function showSingleVenue(aVenue) {
 
     clone2.querySelector(".readmore a").href="form.html?id=" + aVenue.id;
     clone2.querySelector(".evt-btn a").textContent= venText.toUpperCase() + " EVENTS";
+
+
+
+//    clone.querySelector(".venue-text").addEventListener("click", (e)=>{
+//        if (aVenue.slug.toUpperCase() == aVenue.title.rendered){
+//             //console.log(aVenue.slug.toUpperCase() == aVenue.title.rendered)
+//      clone2.querySelector(".concert-modal").classList.add("hide")
+//         }
+//
+//    })
+
     concVenues.appendChild(clone2);
-    //call the function here to avoid set timeout
-    btnLoop();
+    //call the function here to avoid set timeout, don't its in the loop, executed multiple time
+    //btnLoop();
+    //btnHighlite();
+
 
 }
 fetchVenues();
 
-//setTimeout(btnLoop, 2000)
+setTimeout(btnLoop, 1000)
 // make user click on the buttons(.venue-text) and it displays only that venue
 
 function btnLoop() {
-    let filterBtn = document.querySelectorAll(".venue-box")
+    let filterBtn = document.querySelectorAll(".venue-text")
    filterBtn.forEach((x)=>{
     x.addEventListener("click", filterVenue)
-   })
-}
-    function filterVenue(e){
+  //console.log(x)
+//        x.classList.remove("white");
+//       x.parentElement.classList.remove("highlited")
+
+
+       function filterVenue(e){
+           let section = e.target.parentElement.parentElement.parentElement;
+           //console.log(section.childElementCount)
+           //console.log(section.children[i=0; i < section.childElementCount; i++].children[0])
+
+          var children = Array.from(section.children);
+           var child;
+           for (child in children){
+               //let buttons = children[child].getElementsByTagName("button")
+               console.log(children[child].getElementsByTagName("button")[0].childNodes[1])
+               console.log(children[child].getElementsByTagName("button")[0])
+
+             children[child].getElementsByTagName("button")[0].classList.remove("highlited");
+             children[child].getElementsByTagName("button")[0].childNodes[1].classList.remove("white");
+
+
+           }
+
        // console.log(e.target.textContent)
-
-
-
         let article = document.querySelectorAll("article")
-
         article.forEach((a)=>{
 
         if (e.target.textContent == a.querySelector('h1').textContent) {
-            console.log(e.currentTarget)
-            console.log(e.target)
+            //console.log(e.currentTarget)
+
+//            console.log("true")
             e.target.classList.toggle("white")
-        e.target.parentElement.classList.toggle("highlited");
-
-
+       e.target.parentElement.classList.toggle("highlited");
             a.classList.remove("hide");
-
         }
         else{
                 a.classList.add("hide")
             }
-
      })
-
     }
 
 
 
-////change button color and filter content on the click
-//let filterBtn = document.querySelector(".readmore")
-//
-//filterBtn.addEventListener("click", ()=>{
-//    filterBtn.classList.add("highlited");
-//    if(filterBtn.classList.contains("highlited")==){
-//
-//    }
-//
-//
-//})
 
-//buttons animation
 
-//if ($('.readmore').hasClass('highlighted') == false) {
-//        $('.readmore').addClass('highlighted') }else {$('.readmore').removeClass('highlighted')}
-//
-//    if ($('.readmore').hasClass('highlighted')) {
-//        $('.readmore').removeClass('highlighted')
-//    }
 
-//function filter(myFilter) {
-//    document.querySelectorAll(".concert-modal").forEach(section => {
-//        if (venText == aVenue.title) {
-//            section.classList.remove("hide");
-//        } else {
-//            section.classList.add("hide");
-//        }
-//    })
-//}
+   })
+}
+
+
+
+
+
+
